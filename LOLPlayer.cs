1b31030edd65372358f3ad11ef41048b39e2fd0d
barbara <bstimac9009@gmail.com>
2024-05-28 13:25:13 +0200

    commit

---
+++ LOLPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class LOLPlayer : IPlayerAction
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Score { get; set; }
+        public int Level { get; set; }
+        public abstract void TakeDamage(int damage);
+        public abstract void SpecialMove();
+
+        public LOLPlayer(string name)
+        {
+            Name = name;
+            Health = 2000;
+            Score = 0;
+            Level = 0;
+        }
+
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+
+        public bool HasSpecialMove()
+        {
+            if (Level > 6)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+
+
+        public void LevelUp()
+        {
+            Level++;
+            Console.WriteLine($"{Name} has leveled up to level {Level}!");
+        }
+    }
+}
---
+++ LuxPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class LuxPlayer : LOLPlayer
+    {
+        override public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health < 0)
+            {
+                Health = 0;
+                 = false;
+            }
+        }
+
+        override public void SpecialMove()
+        {
+            if (base.HasSpecialMove())
+            {
+                Console.WriteLine($"{Name} is using special move");
+                Health -= 1000;
+            }
+
+        }
+
+
+    }
+}
